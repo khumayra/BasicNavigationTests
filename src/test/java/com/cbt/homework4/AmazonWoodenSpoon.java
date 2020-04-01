030e884dbf6dc99a0c4235597a64958ac1c49d9b
khumayra <khumayra@gmail.com>
2020-04-01 00:24:07 -0400

    Homework 4

--- src/test/java/com/cbt/homework4/AmazonWoodenSpoon.java
+++ src/test/java/com/cbt/homework4/AmazonWoodenSpoon.java
@@ -3,7 +3,9 @@ package com.cbt.homework4;
 import com.cbt.utilities.BrowserFactory;
 import com.cbt.utilities.BrowserUtil;
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -19,61 +21,88 @@ public class AmazonWoodenSpoon {
     private String URL = "https://www.amazon.com/";
     private By searchBy = By.id("twotabsearchtextbox");
     private By searchBtnBy = By.xpath("//input[@value='Go']");
+    private By itemNameBy = By.xpath("//div[@class='a-section a-spacing-medium']//span[@class='a-size-base-plus a-color-base a-text-normal']");
+
+
+    //private By primeLabelBy = By.xpath("//div[@class='a-section a-spacing-medium']//i[@aria-label='Amazon Prime']");
 
     @Test
-    public void searchWoodenSpoonVerify(){
-        driver.findElement(searchBy).sendKeys("wooden spoon");
-        driver.findElement(searchBtnBy).click();
+    public void searchWoodenSpoonVerify() {
+        List<WebElement> list = driver.findElements(itemNameBy);
+        List<WebElement> wholePrice = driver.findElements(By.cssSelector(".a-price-whole"));
+        List<WebElement> fractionPrice = driver.findElements(By.cssSelector(".a-price-fraction"));
+
+        Random r = new Random();
+        int index = r.nextInt(list.size());
+
+        String item = list.get(index).getText().trim();
+        String price = "$" + wholePrice.get(index).getText() + "." + fractionPrice.get(index).getText();
+
+        list.get(index).click();
+
+        String actualItem = driver.findElement(By.id("productTitle")).getText().trim();
+        Assert.assertEquals(actualItem, item);
+
+        String actualPrice = driver.findElement(By.id("priceblock_ourprice")).getText();
+        Assert.assertEquals(actualPrice, price);
+
+        String qty = driver.findElement(By.cssSelector("span[class='a-dropdown-label']+span")).getText().trim();
+        Assert.assertEquals(qty, "1");
+
+        Assert.assertTrue(driver.findElement(By.id("add-to-cart-button")).isDisplayed());
+    }
+
+    @Test
+    public void verifyPrimeAmazonItem(){
+        /*Solution 1
+        List<WebElement> allItems = driver.findElements(allItemsBy);
+        String actualName = "";
+        for (int i = 0; i <allItems.size() ; i++) {
+            WebElement amazonItem = driver.findElement(By.xpath("(//div[@class='a-section a-spacing-medium']//i[@aria-label='Amazon Prime'])"
+                                                                +"["+(i+1)+"]"));
+
+            if(amazonItem!=null){
+                actualName = driver.findElement(By.xpath("(//div[@class='a-section a-spacing-medium']//span[@class='a-size-base-plus a-color-base a-text-normal'])"
+                                    +"["+(i+1)+"]")).getText();
+                System.out.println("actualName = " + actualName);
+                break;
+            }
+        }
+        WebElement primeCheckBox =driver.findElement(By.xpath("//li[@aria-label='Prime Eligible']"));
+        primeCheckBox.click();
         BrowserUtil.wait(5);
-        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String expectedName = driver.findElement(By.xpath("(//span[@class='a-size-base-plus a-color-base a-text-normal'])[1]")).getText().trim();
+        System.out.println("expectedName = " + expectedName);
+        Assert.assertEquals(actualName,expectedName );*/
 
-//      String items = js.executeScript("document.getElementsByClassName('a-size-base-plus a-color-base a-text-normal')").toString();
-//        System.out.println(items);
-//        List<WebElement> prices = driver.findElements(By.className("a-offscreen"));
-//
-//        Random r = new Random();
-//        int index = r.nextInt(list.size());
-//        System.out.println(list.size());
-//        System.out.println(index);
-//
-//        String item = list.get(index).getText().trim();
-//        String itemPrice = "";
-//            if (prices.get(index).getAttribute("data-a-strike").equals("true")){
-//                itemPrice = prices.get(index-1).getText();
-//            } else {
-//                itemPrice = prices.get(index).getText();
-//            }
-//
-//            list.get(index).click();
-//           String actualItem= driver.findElement(By.id("productTitle")).getText().trim();
-//           Assert.assertEquals(actualItem,item);
-//
-//           String actualPrice = driver.findElement(By.id("priceblock_ourprice")).getText();
-//           Assert.assertEquals(actualPrice,itemPrice);
-//
-//           Select select = new Select(driver.findElement(By.cssSelector("span[class='a-dropdown-label']+span")));
-//           String actualQty = select.getFirstSelectedOption().getText().trim();
-//           Assert.assertEquals(actualQty,"1");
-//
-//           Assert.assertTrue(driver.findElement(By.id("add-to-cart-button")).isDisplayed());
-
-
+        //Solution 2
+        String actualName = driver.findElement(By.xpath("(//i[@aria-label='Amazon Prime']/../../../../../..//h2)[1]")).getText();
+        driver.findElement(By.xpath("//li[@aria-label='Prime Eligible']")).click();
+        BrowserUtil.wait(5);
+        String expectedName = driver.findElement(By.xpath("(//span[@class='a-size-base-plus a-color-base a-text-normal'])[1]")).getText().trim();
+        Assert.assertEquals(actualName,expectedName );
+        driver.findElement(By.xpath("//ul[@aria-labelledby='p_89-title']//li[last()]")).click();
+        BrowserUtil.wait(5);
+        String brandSelectedActual = driver.findElement(By.xpath("(//span[@class='a-size-base-plus a-color-base a-text-normal'])[1]")).getText().trim();
+        Assert.assertFalse(brandSelectedActual.equalsIgnoreCase(expectedName));
 
     }
 
     @BeforeMethod
-    public void setup(){
+    public void setup() {
         driver = BrowserFactory.getDriver();
         driver.get(URL);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.findElement(searchBy).sendKeys("wooden spoon");
+        driver.findElement(searchBtnBy).click();
     }
 
     @AfterMethod
-    public void teardown(){
-        if (driver!=null){
+    public void teardown() {
+        if (driver != null) {
             driver.quit();
-            driver=null;
+            driver = null;
         }
     }
 
--- src/test/java/com/cbt/utilities/BrowserUtil.java
+++ src/test/java/com/cbt/utilities/BrowserUtil.java
@@ -1,5 +1,11 @@
 package com.cbt.utilities;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.sql.Driver;
+
 public class BrowserUtil {
    public static void wait(int seconds){
            try{
@@ -8,4 +14,18 @@ public class BrowserUtil {
                e.printStackTrace();
            }
    }
+    /**
+     * waits for backgrounds processes on the browser to complete
+     *
+     * @param timeOutInSeconds
+     */
+    public static void waitForPageToLoad(long timeOutInSeconds) {
+        ExpectedCondition<Boolean> expectation = driver -> ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+        try {
+            WebDriverWait wait = new WebDriverWait(BrowserFactory.getDriver(), timeOutInSeconds);
+            wait.until(expectation);
+        } catch (Throwable error) {
+            error.printStackTrace();
+        }
+    }
 }
